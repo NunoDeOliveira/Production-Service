420e1ce9941a90a51203c30cee3c8451219c8206
vant <ndeoliveira.tlm@google.es>
2026-04-25 13:46:42 +0200

    Solution problem of test configuration

--- src/test/java/com/tfg/productionservice/ProductionServiceApplicationTests.java
+++ src/test/java/com/tfg/productionservice/ProductionServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.tfg.productionservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProductionServiceApplication.class)
 class ProductionServiceApplicationTests {
 
     @Test
